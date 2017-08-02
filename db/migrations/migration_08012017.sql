dbf80b1736d63b92c8f979de51cafa47c5afe72c
Rana Bhattacharyya <ranab33@gmail.com>
2017-08-01 21:58:27 -0400

    database added

---
+++ db/migrations/migration_08012017.sql
@@ -0,0 +1,23 @@
+--\c todo_review_dev;
+
+CREATE TABLE IF NOT EXISTS users
+(
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(255) UNIQUE NOT NULL,
+  password_digest TEXT NOT NULL,
+  first_name VARCHAR(255),
+  last_name VARCHAR(255),
+  email VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS nutritable
+(
+  id SERIAL PRIMARY KEY,
+  food VARCHAR(255),
+  brand VARCHAR(255),
+  calories INTEGER,
+  fiber INTEGER,
+  carbs INTEGER,
+  fat INTEGER,
+  user_id INT REFERENCES users(id)
+);
