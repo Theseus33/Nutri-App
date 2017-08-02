07308d5860f27cae36aba99cc99b3145bde5100e
Rana Bhattacharyya <ranab33@gmail.com>
2017-08-02 09:18:39 -0400

    added user and nutri routes

---
+++ routes/nutri-routes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const nutriRoutes = express.Router();
+const nutriController = require('../controllers/nutri-controller');
+const authHelpers = require('../services/auth/auth-helpers');
+
+nutriRoutes.get('/', authHelpers.loginRequired, nutriController.index);
+nutriRoutes.post('/', authHelpers.loginRequired, nutriController.create);
+
+nutriRoutes.get('/add', (req, res) => {
+  res.render('nutriList/nutri-add', {
+    currentPage: 'add',
+  });
+});
+
+nutriRoutes.get('/:id/edit', authHelpers.loginRequired, nutriController.edit);
+nutriRoutes.get('/:id', authHelpers.loginRequired, nutriController.show);
+nutriRoutes.put('/:id',authHelpers.loginRequired, nutriController.update);
+nutriRoutes.delete('/:id', authHelpers.loginRequired, nutriController.delete);
+
+module.exports = nutriRoutes;
